578676921c551d71db77ccb03dd443a7d629c3aa
mhmddorgham <mohammed.dorgham25@gmail.com>
2024-10-04 23:45:32 +0400

    used RunnableLambda

--- langchain_utils.py
+++ langchain_utils.py
@@ -28,31 +28,25 @@ from operator import itemgetter
 
 from langchain_core.output_parsers import StrOutputParser
 
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 
-from table_details import table_chain as select_table
+
+# from table_details import table_chain as select_table
 from prompts import final_prompt, answer_prompt
 
 import streamlit as st
 @st.cache_resource
 def get_chain():
-    print("Creating chain")
+    print("Creating chain...")
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")    
     llm = ChatOpenAI(api_key= os.getenv("OPENAI_SECRET_KEY"), model="gpt-3.5-turbo", temperature=0)
     generate_query = create_sql_query_chain(llm, db,final_prompt) 
     execute_query = QuerySQLDataBaseTool(db=db)
     rephrase_answer = answer_prompt | llm | StrOutputParser()
-    print("rephrase_answer:: ", rephrase_answer)
-    # chain = generate_query | execute_query
-
     chain = (
-    RunnablePassthrough.assign(table_names_to_use=select_table) |
-    RunnablePassthrough.assign(query=generate_query).assign(
-        result=itemgetter("query") | execute_query
-    )
-    | rephrase_answer
-)
+        RunnableLambda(lambda inputs: {"question": inputs["question"], "query": generate_query.invoke(inputs), "result": execute_query.invoke({"query": generate_query.invoke(inputs)})})
+    ) | rephrase_answer
 
 
     return chain
