f06668ca74624b6fe36ff770fe0fbe226259cf08
mhmddorgham <mohammed.dorgham25@gmail.com>
2024-10-14 02:17:20 +0400

    updates

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ st.title("Natural Language to SQL 🚀")
 
 client = OpenAI(api_key=os.getenv('OPENAI_SECRET_KEY'))
 
-# Set a default model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Initialize chat history
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display previous messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -32,13 +30,9 @@ if prompt := st.chat_input("What is up?"):
     with st.spinner("Generating response..."):
         with st.chat_message("assistant"):
             try:
-                # Attempt to invoke the chain
-                print("st.session_state.messages:: ", st.session_state.messages)
                 response = invoke_chain(prompt, st.session_state.messages)
                 st.markdown(response)
                 st.session_state.messages.append({"role": "assistant", "content": response})
             except Exception as e:
-                # Handle any exception that occurs
                 st.markdown("**Error:** Please provide more details.")
-                # Optionally, log the error for debugging
                 print(f"Error occurred: {e}")
